d042d0a837b8284a9faaf84a9577a712b37ac691
genald-ocampo <ocampogenald@gmail.com>
2024-09-26 18:56:37 -0700

    Payroll Calculator

---
+++ workbook-1/BasicCalculator/src/PayRollCalculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PayRollCalculator {
+public static void main(String[] args) {
+    Scanner scannerObject = new Scanner(System.in);
+    System.out.println("Enter your name: ");
+    String name = scannerObject.nextLine();
+
+    System.out.println("Enter hours worked: ");
+    float hours = scannerObject.nextFloat();
+    scannerObject.nextLine();
+
+    System.out.println("Enter pay rate: ");
+    float payRate = scannerObject.nextFloat();
+    scannerObject.nextLine();
+
+    System.out.println("Hello " + name + "," + " your gross pay is: " + "$" + (hours + payRate));
+
+    }
+}
