089a9a376dea3b606e345e6542d4aa7bafe91970
genald-ocampo <ocampogenald@gmail.com>
2024-10-03 16:49:11 -0700

    updated

---
+++ workbook-2/CellphoneService/src/main/java/com/pluralsight/CellphoneService.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class CellphoneService {
+   static Scanner scanner =
+    public static void main(String[] args) {
+
+    }
+}
--- workbook-2/TheaterReservations/src/TheaterReservations.java
+++ workbook-2/TheaterReservations/src/TheaterReservations.java
@@ -1,15 +1,25 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class TheaterReservations {
     public static void main(String[] args) {
-       Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter full name");
-        String name = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter full name:");
+        String fullName = scanner.nextLine();
+        System.out.println("Enter reservation date (MM/dd/yyyy):");
+        String date = scanner.nextLine();
+        System.out.println("How many tickets would you like?");
+        int tickets = scanner.nextInt();
 
+        DateTimeFormatter info = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        LocalDate reservationDate = LocalDate.parse(date, info);
 
+        String[] names = fullName.split(" ");
+        String usersName = names[1] + ", " + names[0];
 
+        String ticketPrompt = tickets == 1 ? "ticket" : "tickets";
 
-
-
+        System.out.println(tickets + " " + ticketPrompt + "reserved for " + reservationDate.format(info) + " under " + usersName);
     }
 }
